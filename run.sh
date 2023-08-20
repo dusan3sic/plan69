32aacf32557a8becbb8d32938b3e6ee751935aff
Dusan Trisic <dusan3sic@pop-os.localdomain>
2023-08-20 17:56:59 +0200

    added 9front repo in gitignore + qemu runs in zoom to fit mode

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ qemu-system-x86_64 -m 1024 \
 -net nic,model=virtio,macaddr=00:20:91:37:33:77 -net user \
 -device virtio-scsi-pci,id=scsi \
 -drive if=none,id=vd0,file=9front.qcow2.img \
--device scsi-hd,drive=vd0
+-device scsi-hd,drive=vd0 \
+-display gtk,zoom-to-fit=on
 
 return
 
