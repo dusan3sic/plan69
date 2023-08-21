4751e6d9be869ee483374da4a7abd0eef593398d
Dusan Trisic <dusan3sic@pop-os.localdomain>
2023-08-21 20:59:40 +0200

    ne znam brate trenutno nisam ni pokrenuo plan9

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 qemu-system-x86_64 -m 1024 \
 -net nic,model=virtio,macaddr=00:20:91:37:33:77 -net user \
 -device virtio-scsi-pci,id=scsi \
--drive if=none,id=vd0,file=9plan.raw \
+-drive if=none,id=vd0,file=hdd/9plan.raw \
 -device scsi-hd,drive=vd0 \
 -display gtk,zoom-to-fit=on
 
