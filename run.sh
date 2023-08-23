d291e82b74e9be19a4cd34d6fb3ef67821d6f7fd
Dusan Trisic <dusan3sic@pop-os.localdomain>
2023-08-23 10:04:34 +0200

    otvoreni su portovi, dodata skripta koja dize tap interface

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-#! /bin/zsh
+#!/bin/bash
 
-qemu-system-x86_64 -m 1024 \
--net nic,model=virtio,macaddr=00:20:91:37:33:77 -net user \
+ qemu-system-x86_64 -cpu host -enable-kvm -m 1024 \
+-netdev tap,id=eth,ifname=tap0,script=no,downscript=no \
+-device e1000,netdev=eth,mac=52:54:00:00:ee:03 \
 -device virtio-scsi-pci,id=scsi \
--drive if=none,id=vd0,file=hdd/9plan.qcow2,format=qcow2 \
+-drive if=none,id=vd0,file=hdd/9plan.qcow2 \
 -device scsi-hd,drive=vd0 \
 -display gtk,zoom-to-fit=on
-
---
+++ tapSet.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+sudo ip tuntap add dev tap0 mode tap user dusan3sic
+sudo ip address add 10.0.0.1/24 dev tap0
+sudo ip link set tap0 up
