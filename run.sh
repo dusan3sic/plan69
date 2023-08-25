35f505a05681c2512c76810988e2d27fa05bfd58
Dusan Trisic <dusan3sic@pop-os.localdomain>
2023-08-25 10:21:16 +0200

    izbrisao sam edf fajlove

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
- qemu-system-x86_64 -cpu host -enable-kvm -m 1024 \
+qemu-system-x86_64 -cpu host -enable-kvm -m 1024 \
 -netdev tap,id=eth,ifname=tap0,script=no,downscript=no \
 -device e1000,netdev=eth,mac=52:54:00:00:ee:03 \
 -device virtio-scsi-pci,id=scsi \
--- tapSet.sh
+++ tapSet.sh
@@ -3,3 +3,4 @@
 sudo ip tuntap add dev tap0 mode tap user dusan3sic
 sudo ip address add 10.0.0.1/24 dev tap0
 sudo ip link set tap0 up
+
